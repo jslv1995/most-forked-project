7666ca7ca4890c9fec7e6ced8cba670f71ea3a19
John Lewis <jslv1995@gmail.com>
2024-08-18 15:24:23 -0400

    added plotly.express functionality to visualize data

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-## Charting the Top 10 "Most Forked" Github repositories
+## Charting the Top 30 "Most Forked" Github repositories.
 
 import requests
+import plotly.express as px
 
-# Make the API call
+# Make the API call.
 url = "https://api.github.com/search/repositories"
-url += "?q=language:python+forks:>100&sort=forks&order=desc&per_page=10"
+url += "?q=language:python+forks:>100&sort=forks&order=desc&per_page=30"
 
 headers = {"Accept": "application/vnd.github.v3+json"}
 response = requests.get(url, headers=headers)
@@ -16,11 +17,18 @@ if response.status_code == 200:
 else:
     print(f"Failed to retrieve data: Status code {response.status_code}")
 
-print("\nSelected information about each repository:")
-# Loop over each result to gather necessary information
-for repo_dict in repo_dicts:
-    print(f"\nName: {repo_dict['name']}")
-    print(f"Description: {repo_dict['description']}")
-    print(f"Fork Count: {repo_dict['forks_count']}")
-    print(f"Created: {repo_dict['created_at']}")
-    print(f"Repository: {repo_dict['html_url']}")
+# Process repositories.
+repo_names, forks, descriptions = [], [], []
+for repo in repo_dicts:
+    repo_names.append(repo['name'])
+    forks.append(repo['forks_count'])
+    descriptions.append(repo['description'])
+
+# Visualize results.
+title = "30 Most-Forked Github Repositories"
+labels = {'x': 'Repositories', 'y': 'Forks'}
+fig = px.bar(x=repo_names, y=forks, title=title, labels=labels, hover_name=descriptions)
+
+fig.update_layout(title_font_size=20, xaxis_title_font_size=15, yaxis_title_font_size=15)
+
+fig.show()
